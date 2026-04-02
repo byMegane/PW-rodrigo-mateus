3a45a94c7207f5c82f67f5bd47f4d955cc88f3ca
Rodrigo Mateus <rodrigoglasses@gmail.com>
2026-04-02 11:54:23 -0300

    Add PHP files for student information display

---
+++ Aula 08/Ex2.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = "Rafael Ramos KokeGames"
+$idade = 56;
+$matriculaAtiva = true;
+
+echo "Aluno(a): " . $nome;
+echo "\nIdade: " . $idade;
+?>
---
+++ Aula 08/Ex3.php
@@ -0,0 +1,10 @@
+<?php
+
+$nome = "Rodrigo";
+$curso = "Desenvolvimento de Sistemas";
+$semestre = 2;
+
+echo "Nome do Aluno: " . $nome . "<br>";
+echo "Curso: " . $curso . "<br>";
+echo "Semestre: " . $semestre . "° semestre";
+?>
